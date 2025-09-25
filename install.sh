030f6fbc8680d847144e70c789dd7326f09cc45f
zedonarch <zedonix@proton.me>
2025-09-25 13:10:02 +0530

    testing

--- install.sh
+++ install.sh
@@ -259,9 +259,9 @@ fi
 # Pacstrap with error handling
 if [[ "$ddos" == "no" ]]; then
   reflector --country 'India' --latest 40 --age 6 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-  sed -i '/niranjan/d' /etc/pacman.d/mirrorlist
-  sed -i '1i Server = https://mirror.sahil.world/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
-  sed -i '1i Server = https://mirrors.abhy.me/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
+  # sed -i '/niranjan/d' /etc/pacman.d/mirrorlist
+  # sed -i '1i Server = https://mirror.sahil.world/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
+  # sed -i '1i Server = https://mirrors.abhy.me/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
 else
   cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
   cat >/etc/pacman.d/mirrorlist <<'EOF'
